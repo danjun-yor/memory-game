1e188f2d3a5d0ab81d26402d80ff43da57afb7f0
danjun <chljy33@naver.com>
2019-10-13 00:31:15 +0900

    Updates

---
+++ precache-manifest.15b646e955ed2c22c1854611b98dbcd2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "284194c490db7f95c0befe60411ee6e6",
+    "url": "/memory-game/index.html"
+  },
+  {
+    "revision": "177e00f95787e1a9dfdb",
+    "url": "/memory-game/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "23c7904c9aa09dcf8b3a",
+    "url": "/memory-game/static/css/main.4ee91ddf.chunk.css"
+  },
+  {
+    "revision": "177e00f95787e1a9dfdb",
+    "url": "/memory-game/static/js/2.33bcf436.chunk.js"
+  },
+  {
+    "revision": "23c7904c9aa09dcf8b3a",
+    "url": "/memory-game/static/js/main.ce2b9877.chunk.js"
+  },
+  {
+    "revision": "d7090f0a0e3076032063",
+    "url": "/memory-game/static/js/runtime-main.0c611f47.js"
+  }
+]);
