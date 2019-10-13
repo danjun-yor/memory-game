b2c64abaeb920b47e90251eb109f7a877f06f65e
danjun <chljy33@naver.com>
2019-10-14 04:33:47 +0900

    카드 체크됐을 때 스타일 추가

--- src/Components/Card/index.tsx
+++ src/Components/Card/index.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { tCard } from "../Deck";
+import "./styles.scss";
 
 interface Props {
   size: number;
@@ -14,12 +15,14 @@ export default class Card extends Component<Props, State> {
   getClassName() {
     const {
       size,
-      card: { isFlipping, isOpened, shouldShow, card }
+      card: { isFlipping, isOpened, isChecked, shouldShow, card }
     } = this.props;
 
     return `card x${size} ${
       isFlipping ? (isOpened ? "reverse-flip" : "flip") : ""
-    } ${shouldShow || !card ? "show open" : ""} ${!card ? "disabled" : ""}`;
+    }${shouldShow || !card ? " show open" : ""}${!card ? " disabled" : ""}${
+      isChecked ? " checked" : ""
+    }`;
   }
 
   render() {
@@ -28,7 +31,10 @@ export default class Card extends Component<Props, State> {
     return (
       <li
         className={this.getClassName()}
-        onClick={() => handleCardClick(card.id)}
+        onClick={(e: React.MouseEvent) => {
+          // const target = e.target as HTMLElement;
+          handleCardClick(card.id);
+        }}
       >
         {card["card"] ? <FontAwesomeIcon icon={card.card} /> : ""}
       </li>
