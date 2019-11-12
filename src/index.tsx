3cd389385d21f091cb4b065e3ca1a8eea47c731c
Junyeol Lee <jylee@rsupport.com>
2019-11-12 19:46:19 +0900

    apollo client & graphql install

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
+import { ApolloProvider } from "react-apollo-hooks";
 import "normalize.css";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+import Client from "./Apollo/Client";
 
 library.add(fas);
-ReactDOM.render(<App />, document.getElementById("root"));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+  <ApolloProvider client={Client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
