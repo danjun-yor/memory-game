11ee1c12d22b5191e6284331d27ef04725ff5688
Junyeol <jylee@rsupport.com>
2020-01-05 17:28:03 +0900

    prevent multiple siginin request

--- src/Pages/SignIn/index.tsx
+++ src/Pages/SignIn/index.tsx
@@ -1,18 +1,18 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
+import { useHistory } from "react-router-dom";
 import "./styles.scss";
 import MyClient from "../../MyClient";
 
 export default () => {
   const emailRef = useRef<HTMLInputElement>(null);
   const passwordRef = useRef<HTMLInputElement>(null);
+  const history = useHistory();
+
+  let isSubmitting = false;
 
   const onSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
 
-    signIn();
-  };
-
-  const signIn = async () => {
     const email = emailRef.current!.value;
     const password = passwordRef.current!.value;
     if (!email) {
@@ -26,11 +26,17 @@ export default () => {
       return;
     }
 
+    if (isSubmitting) {
+      return;
+    }
+
+    isSubmitting = true;
     const { data, errors } = await MyClient.requestWithGraphQL(`mutation { 
       signIn(email: "${email}", password: "${password}") {
         token
       }
    }`);
+    isSubmitting = false;
 
     if (errors) {
       alert(errors[0].message);
@@ -38,6 +44,8 @@ export default () => {
     }
     const { token } = data;
     localStorage.setItem("token", token);
+
+    history.push("/");
   };
 
   return (
